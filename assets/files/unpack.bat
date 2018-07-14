073087c91aca67bf570f48d4aa85bbe360e190c2
Plailect <plailect@gmail.com>
2018-07-13 20:21:19 -0400

    correct the order of this

--- assets/files/unpack.bat
+++ assets/files/unpack.bat
@@ -1,8 +1,5 @@
 @echo off
 pkg2zip -x *.pkg
-robocopy app\PCSG90096\ app\ux0_temp_game_PCSG90096_app_PCSG90096 /E /MOV
-copy app\ux0_temp_game_PCSG90096_app_PCSG90096\sce_sys\package\temp.bin license\ux0_temp_game_PCSG90096_license_app_PCSG90096\6488b73b912a753a492e2714e9b38bc7.rif
-
 del /Q app\PCSG90096\resource\movie\*
 del /Q app\PCSG90096\resource\image\bg\*
 del /Q app\PCSG90096\resource\image\ev\*
@@ -14,3 +11,5 @@ del /Q app\PCSG90096\resource\sound\se\*
 del /Q app\PCSG90096\resource\sound\sec\*
 del /Q app\PCSG90096\resource\sound\voice\*
 del /Q app\PCSG90096\resource\text\01\*
+robocopy app\PCSG90096\ app\ux0_temp_game_PCSG90096_app_PCSG90096 /E /MOV
+copy app\ux0_temp_game_PCSG90096_app_PCSG90096\sce_sys\package\temp.bin license\ux0_temp_game_PCSG90096_license_app_PCSG90096\6488b73b912a753a492e2714e9b38bc7.rif
