980a1851de8ab937a2b7ea1f5198562e77d7bd6b
emiyl <me@emiyl.com>
2021-11-03 15:35:47 +0000

    vuepress update

---
+++ docs/.vuepress/configs/en_US.js
@@ -0,0 +1,84 @@
+module.exports = {
+  search: 'Search',
+  
+  locales: {
+    lang: 'en-US',
+    title: 'PS Vita (TV) Hacks Guide',
+    description: 'A complete guide to PS Vita (TV) custom firmware, from stock to Ensō.'
+  },
+  
+  themeConfig: {
+  	selectLanguageName: "English",
+
+  	backToHome: "Take me home",
+  	contributorsText: "Contributors",
+  	editLinkText: "Edit this page",
+  	lastUpdatedText: "Last Updated",
+  	openInNewWindow: "Open in new window",
+  	selectLanguageAriaLabel: "Select language",
+  	toggleDarkMode: "Toggle dark mode",
+  	toggleSidebar: "Toggle sidebar",
+  	discordNoticeText: "For support in English, ask for help at [HENkaku on Discord](https://discord.gg/m7MwpKA).",
+      
+    navbar: [
+      {
+        text: 'Guides',
+        children: [
+          '/adrenaline',
+          {
+            text: 'SD2Vita',
+            link: '/yamt'
+          },
+        ]
+      },
+      {
+        text: 'Help',
+        children: [
+          '/troubleshooting',
+          '/faq',
+          {
+            text: 'Discord',
+            link: 'https://discord.gg/m7MwpKA'
+          },
+        ]
+      },
+      {
+        text: 'Site Info',
+        children: [
+          '/donations',
+          '/credits',
+          '/site-navigation'
+        ]
+      },
+    ],
+      
+    sidebar: {
+      '/': [
+        {
+          text: 'Guide',
+          children: [
+            '/',
+            '/get-started',
+            '/updating-firmware-(3.73)',
+            '/installing-h-encore',
+            '/installing-enso',
+            '/finalizing-setup'
+          ],
+        },
+      ],
+      
+      '/installing-henkaku': [
+        {
+          text: 'Guide',
+          children: [
+            '/',
+            '/get-started',
+            '/installing-henkaku',
+            '/installing-enso',
+            '/finalizing-setup'
+          ],
+        },
+      ],
+    },
+  }
+};
