126acb3481d2a94cc381c661fe8f13297c4f0a7b
emiyl <me@emiyl.com>
2022-08-29 17:16:33 +0100

    new ad units

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,6 @@
 const config = require("./configs");
 const path = require("path");
 
-const adArr = [
-  { slot: '/22046652915/vita-0', size: [[728, 90], [320, 50]], id: 'div-gpt-ad-1649073007284-0' },
-  { slot: '/22046652915/vita-1', size: [[728, 90], [320, 50]], id: 'div-gpt-ad-1649073038121-0' }
-]
-
 module.exports = {
   locales: {
     '/': config.en_US.locales
@@ -32,7 +27,10 @@ module.exports = {
 		docsBranch: "master",
 		docsDir: "docs",
     selectLanguageText: '<i class="fas fa-globe"/>',
-    adArr: adArr,
+    adUnits: [
+      "5340",
+      "5342"
+    ],
     locales: {
       '/': config.en_US.themeConfig
     },
@@ -54,10 +52,9 @@ module.exports = {
     [ 'meta', { name: 'msapplication-config', content: '/assets/images/browserconfig.xml?v=PYEmwKvQAx' } ],
     [ 'meta', { name: 'theme-color', content: '#2E3440' } ],
     [ 'link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' } ],
-    [ 'script', { src: 'https://cdn.thisiswaldo.com/static/js/5303.js' } ],
+    [ 'script', { type: 'text/javascript', src: '//cdn.thisiswaldo.com/static/js/5303.js' } ],
     [ 'script', { src: 'https://www.googletagmanager.com/gtag/js?id=UA-152619365-1'} ],
 		[ 'script', {}, 'function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-152619365-1");' ],
-    require('./plugins/createAdScript/lib/')(adArr)
   ],
 
   theme: path.resolve(__dirname, './vuepress-theme'),
